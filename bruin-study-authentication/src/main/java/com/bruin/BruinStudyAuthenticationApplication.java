dd881473619995fedebb77648272cb43656d3e5f
xiowewe <xiowewe@163.com>
2019-12-25 23:33:15 +0800

    test

--- bruin-study-authentication/src/main/java/com/bruin/BruinStudyAuthenticationApplication.java
+++ bruin-study-authentication/src/main/java/com/bruin/BruinStudyAuthenticationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BruinStudyAuthenticationApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BruinStudyAuthenticationApplication.class, args);
     }
 
