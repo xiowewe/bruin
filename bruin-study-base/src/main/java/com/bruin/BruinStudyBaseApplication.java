b566ffc831fb48722e4f6fdc57a4b311ad8466e7
xiowewe <xiowewe@163.com>
2019-12-25 23:37:10 +0800

    java base

---
+++ bruin-study-base/src/main/java/com/bruin/BruinStudyBaseApplication.java
@@ -0,0 +1,13 @@
+package com.bruin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruinStudyBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruinStudyBaseApplication.class, args);
+    }
+
+}
