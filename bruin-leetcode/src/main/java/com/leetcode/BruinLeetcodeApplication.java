bdb8f7d81f75b9c704e0ae891968a6d4ba9e64ac
xiongwenwen <xiongwenwen@seeapp.com>
2020-01-02 16:57:36 +0800

    leetcode

---
+++ bruin-leetcode/src/main/java/com/leetcode/BruinLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruinLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruinLeetcodeApplication.class, args);
+    }
+
+}
