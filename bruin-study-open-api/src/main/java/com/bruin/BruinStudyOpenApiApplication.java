ab029c4856d4a3441c10c4eaea2f144d302649ec
xiongwenwen <xiongwenwen@seeapp.com>
2020-04-21 15:35:59 +0800

    maven 依赖修改

---
+++ bruin-study-open-api/src/main/java/com/bruin/BruinStudyOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.bruin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruinStudyOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruinStudyOpenApiApplication.class, args);
+    }
+
+}
