cac03bf661a75a626a3003f693924a77fe9c55d1
xiongwenwen <xiongwenwen@seeapp.com>
2020-04-21 15:44:23 +0800

    bruin-reactor

---
+++ bruin-study-reactor/src/main/java/com/bruin/reactor/BruinStudyReactorApplication.java
@@ -0,0 +1,13 @@
+package com.bruin.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruinStudyReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruinStudyReactorApplication.class, args);
+    }
+
+}
---
+++ bruin-study-reactor/src/test/java/com/bruin/reactor/BruinStudyReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruin.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruinStudyReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
