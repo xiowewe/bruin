6e1e395b282facd29e78d37b3e0e7debb292e6cb
see-bruin <xiongwenwen@seeapp.com>
2020-06-26 15:22:47 +0800

    增加saga项目

---
+++ bruin-study-saga/src/main/java/com/bruin/SagaApplication.java
@@ -0,0 +1,12 @@
+package com.bruin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SagaApplication.class, args);
+    }
+
+}
